cdb85ad949386df663614727a94ea5823c4a9dc9
Manjesh Prasad <manjeshprasad@Manjeshs-Air.hsd1.ca.comcast.net>
2024-02-09 14:40:34 -0800

    Made some updates using the JQuerry frameword

--- index.js
+++ index.js
@@ -1,65 +1,78 @@
 
 
+
+/* This is without the Jquerry syntax  */
 // Button Press
-for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
-document.querySelectorAll("button")[i].addEventListener("click", function () { 
-    
-    let buttonInnerHTML = this.innerHTML
-   
-   makeSound(buttonInnerHTML)
-   buttonAnimation(buttonInnerHTML)
- 
+// for(let i = 0; i < document.querySelectorAll(".drum").length; i++){
+// document.querySelectorAll("button")[i].addEventListener("click", function () {
+//
+//     let buttonInnerHTML = this.innerHTML
+//
+//    makeSound(buttonInnerHTML)
+//    buttonAnimation(buttonInnerHTML)
+//
+// })
+// }
+
+$("button").click(function() {
+
+  let button = $(this).html()
+  makeSound(button)
+  buttonAnimation(button)
 })
-}
 
 
 // Keyboard press
-document.addEventListener("keypress", function(event) { 
-    makeSound(event.key)
-    buttonAnimation(event.key)
-
+/* This is without Jquerry framework *****************/
+// document.addEventListener("keypress", function(event) {
+//     makeSound(event.key)
+//     buttonAnimation(event.key)
+//
+// })
+$(document).keypress(function(event){
+  makeSound(event.key)
+  buttonAnimation(event.key)
 })
 
-function makeSound(key){ 
-    switch(key){ 
-        case "w": 
+
+function makeSound(key){
+    switch(key){
+        case "w":
             let tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
           break;
-        case "a":    
+        case "a":
             let tom2 = new Audio("sounds/tom-2.mp3")
             tom2.play();
         break;
-        case "s": 
+        case "s":
             let tom3 = new Audio("sounds/tom-3.mp3")
             tom3.play()
-        break; 
-        case "d": 
+        break;
+        case "d":
             let tom4 = new Audio("sounds/tom-4.mp3")
             tom4.play();
         break
-        case "j": 
+        case "j":
         let snare = new Audio("sounds/snare.mp3")
         snare.play()
-         break 
-    case "k": 
+         break
+    case "k":
         let crash = new Audio("sounds/crash.mp3")
         crash.play();
         break
-    case "l": 
+    case "l":
         let kick = new Audio("sounds/kick-bass.mp3")
         kick.play()
     break
-    
+
     }
  }
 
 
- function buttonAnimation(key){ 
-   var activeButton =  document.querySelector("." + key)
-   activeButton.classList.add("pressed")
-
-    setTimeout(function() { 
-        activeButton.classList.remove("pressed")
+ const buttonAnimation = (key)=> {
+   let select = $("."+key).addClass("pressed")
+    setTimeout(function() {
+        select.removeClass("pressed")
     }, 100)
  }
